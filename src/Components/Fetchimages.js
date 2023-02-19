e0bb8f1d9a9115e761500b80b0d7370959b4b165
Rahul K G <kgrahul96@gmail.com>
2023-02-19 21:34:27 +0530

    added zoom for images

--- src/Components/Fetchimages.js
+++ src/Components/Fetchimages.js
@@ -4,7 +4,7 @@ export const searchImages = async (searchTerm) => {
   const headers = {
     Authorization: `Client-ID Tn0M3jozghR4Y1FYHR9shAKiYIintWx5CdoqTeqwouU`
 };
-//console.log(process.env.REACT_APP_API_KEY)
+
 
  const response = await axios.get(`https://api.unsplash.com/search/photos?query=${searchTerm}`,{headers})
   .then(response =>  
